e0f8592b409cce3902c63a0b30a2fca691e861eb
abhishek49-a <59136068+abhishek49-a@users.noreply.github.com>
2019-12-22 15:58:32 +0530

    comment add

--- src/BasicSelenium/Locators.java
+++ src/BasicSelenium/Locators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
-
+//class
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Downloads\\extr file\\chromedriver_win32\\chromedriver.exe");
